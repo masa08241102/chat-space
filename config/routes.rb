5a925549b3f11b48b9dc4835a92921c6aa303734
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-22 16:23:50 +0900

    groupunction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "messages#index"
+  root to: "groups#index"
   resources :messages
   resources :users, only: [:edit, :update]#user編集機能
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end

3ce8f13ad5d9bc64dc7851983dda2ea219c5ef5e
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-23 12:53:49 +0900

    メッセージ送信機能実施

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,14 @@
 class Group < ApplicationRecord
+  has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?#last_messageにmeassges.lastを代入する。プラス投稿されているか確認する。
+      last_message.content? ? last_message.content: '画像が投稿されています'#三項演算子を使用する
+    else
+      'まだメッセージは投稿されていません'
+    end
+  end
 end

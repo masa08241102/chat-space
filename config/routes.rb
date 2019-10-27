541b1157b1e6832306a1f412b565d13f1d2be047
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-27 17:29:01 +0900

    message.api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "groups#index"
+  root "groups#index"
   resources :messages
   resources :users, only: [:edit, :update]#user編集機能
   resources :groups, only: [:index, :new, :create, :edit, :update] do

9b082e5b7f3ad37cec4a9aae4fcf17d480f64fbc
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-27 19:28:48 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-    if ( message.image ) {
+    var image = message.image ? message.image : '' ;
       var html =
       `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
@@ -16,28 +16,9 @@ $(function(){
               ${message.content}
             </p>
           </div>
-          <img src=${message.image} >
+          <img src=${image} >
         </div>`
       return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
   }
   $('.new_message').on('submit', function(e){
     e.preventDefault();
@@ -54,7 +35,7 @@ $(function(){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});//投稿したら下にスクロールする
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     $('form')[0].reset();//設定した初期値になる
   })
     .fail(function(){

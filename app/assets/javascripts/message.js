b83ae8ca03cb9287cc330d204e5f4012c3d23cd5
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-30 14:21:45 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,7 +48,6 @@ $(function(){
 
   var reloadMessages = function(){//自動更新
     last_message_id = $('.message').last().data("message-id");
-    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'get',

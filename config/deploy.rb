899f92b2a0630c37bb8079a4e5829166cab3c519
masa08241102 <54658461+masa08241102@users.noreply.github.com>
2019-10-31 18:12:34 +0900

    自動デプロイ2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:masa08241102/chat-space"
+set :repo_url,  'git@github.com:masa08241102/chat-space.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
